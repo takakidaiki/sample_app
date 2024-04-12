3975517280333d0b4581930e5dc88f2a1182a04c
daikitakaki <162309554+takakidaiki@users.noreply.github.com>
2024-04-12 12:39:18 +0000

    4-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -35,6 +35,15 @@ class ListsController < ApplicationController
     list.destroy  # データ（レコード）を削除
     redirect_to '/lists'  # 投稿一覧画面へリダイレクト
   end
+  
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end  
+  end
 
   private
   # ストロングパラメータ
