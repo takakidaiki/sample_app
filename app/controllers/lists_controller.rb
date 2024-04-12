4a39e8fc98f8655de714c53b220032d9c96f686f
daikitakaki <162309554+takakidaiki@users.noreply.github.com>
2024-04-12 10:04:21 +0000

    　4-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,7 +2,7 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
-  
+
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
@@ -11,21 +11,34 @@ class ListsController < ApplicationController
     # 4. トップ画面へリダイレクト
     redirect_to list_path(list.id)
   end
-  
+
   def index
     @lists = List.all
-  end 
-  
-  def show 
+  end
+
+  def show
     @list = List.find(params[:id])
   end
-  
+
   def edit
-  end 
-  
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
+  end
+
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
